dcc8473e06791800e0bbdfb7e5735b99eded0888
siyathokoza cyril mbatha <siyathokozacmbatha@gmail.com>
2021-10-27 00:53:02 +0200

    migrated firebase messaging

--- lib/Notifications/PushNotificationsManager.dart
+++ lib/Notifications/PushNotificationsManager.dart
@@ -1,22 +1,22 @@
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationsManager {
-
   PushNotificationsManager._();
 
   factory PushNotificationsManager() => _instance;
 
-  static final PushNotificationsManager _instance = PushNotificationsManager._();
+  static final PushNotificationsManager _instance =
+      PushNotificationsManager._();
 
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
   bool _initialized = false;
 
   Future<void> init() async {
     if (!_initialized) {
       // For iOS request permission first.
-      _firebaseMessaging.requestNotificationPermissions();
-      _firebaseMessaging.configure();
+
+      _firebaseMessaging.requestPermission();
+      // _firebaseMessaging.configure(); //removed or deprecated
 
       // For testing purposes print the Firebase Messaging token
       String token = await _firebaseMessaging.getToken();
