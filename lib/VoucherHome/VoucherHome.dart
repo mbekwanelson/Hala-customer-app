6bd832e3df2fe6abc3040afd077426643ac2596d
siyathokoza cyril mbatha <siyathokozacmbatha@gmail.com>
2021-10-27 01:31:07 +0200

    image picker

--- lib/VoucherHome/VoucherHome.dart
+++ lib/VoucherHome/VoucherHome.dart
@@ -5,8 +5,6 @@ import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mymenu/VoucherHome/Uploader.dart';
 
-
-
 //will allow us to capture an image from camera and allow user to crop and resize it
 class ImageCapture extends StatefulWidget {
   @override
@@ -14,12 +12,13 @@ class ImageCapture extends StatefulWidget {
 }
 
 class _ImageCaptureState extends State<ImageCapture> {
-  File _imageFile;//the file/photo
+  File _imageFile; //the file/photo
 
   //allows user to select image from camera or gallery
   // source = camera/gallery
-  Future<void> _pickImage(ImageSource source) async{
-    File selected = await ImagePicker.pickImage(source:source);//allows user to either select image from gallery or capture a new one with a camera
+  Future<void> _pickImage(ImageSource source) async {
+    File selected = await ImagePicker().pickImage(source: source)
+        as File; //allows user to either select image from gallery or capture a new one with a camera
 
     setState(() {
       _imageFile = selected;
@@ -27,7 +26,7 @@ class _ImageCaptureState extends State<ImageCapture> {
   }
 
   //allows user to crop an image
-  Future<void> _cropImage() async{
+  Future<void> _cropImage() async {
     File cropped = await ImageCropper.cropImage(
       sourcePath: _imageFile.path,
 //      toolbarColor: Colors.purple,
@@ -35,9 +34,11 @@ class _ImageCaptureState extends State<ImageCapture> {
 //      toolbarTitle:"Crop it "
     );
     setState(() {
-      _imageFile  = cropped ?? _imageFile; // if user cancels modifications return the origional image file (uncropped)
+      _imageFile = cropped ??
+          _imageFile; // if user cancels modifications return the origional image file (uncropped)
     });
   }
+
   // Remove image
   void _clear() {
     setState(() => _imageFile = null);
@@ -46,82 +47,81 @@ class _ImageCaptureState extends State<ImageCapture> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child:Row(
+        bottomNavigationBar: BottomAppBar(
+            child: Row(
           children: <Widget>[
             IconButton(
-              onPressed: (){
+              onPressed: () {
                 return _pickImage(ImageSource.camera);
               },
-              icon:Icon(Icons.photo_camera),
+              icon: Icon(Icons.photo_camera),
             ),
             IconButton(
-              onPressed: (){
+              onPressed: () {
                 return _pickImage(ImageSource.gallery);
               },
               icon: Icon(Icons.photo_library),
             ),
           ],
-        )
-      ),
-      body:Column(
-        children: <Widget>[
-          ClipPath(
-            clipper:MyClipper(),
-            child: Container(
-              height:320,
-              decoration: BoxDecoration(
-                color:Colors.red,
-              ),
-              child: Center(
-                child: Text(
-                  "Voucher",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 45,
+        )),
+        body: Column(
+          children: <Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                height: 320,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: Center(
+                  child: Text(
+                    "Voucher",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 45,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              child: ListView(
-                children: <Widget>[
-                  if(_imageFile != null) ...[
+            Expanded(
+              child: Container(
+                child: ListView(children: <Widget>[
+                  if (_imageFile != null) ...[
                     Image.file(_imageFile),
                     Row(
-                    children: <Widget>[
-                      FlatButton(
-                        onPressed: _cropImage,
-                        child: Icon(Icons.crop),
-                      ), FlatButton(
-                        child: Icon(Icons.refresh),
-                        onPressed: _clear,
-                      ),
-                    ],
+                      children: <Widget>[
+                        FlatButton(
+                          onPressed: _cropImage,
+                          child: Icon(Icons.crop),
+                        ),
+                        FlatButton(
+                          child: Icon(Icons.refresh),
+                          onPressed: _clear,
+                        ),
+                      ],
                     ),
-              Uploader(file: _imageFile)//
-                ],
-              ]),
+                    Uploader(file: _imageFile) //
+                  ],
+                ]),
+              ),
             ),
-          ),
-        ],
-      )
-    );
+          ],
+        ));
   }
 }
 
-class MyClipper extends CustomClipper<Path>{
+class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = new Path();
-    path.lineTo(0, size.height-50);
-    var controllPoint = Offset(50,size.height);
-    var endPoint = Offset(size.width/2,size.height);
-    path.quadraticBezierTo(controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
-    path.lineTo(size.width,size.height);
+    path.lineTo(0, size.height - 50);
+    var controllPoint = Offset(50, size.height);
+    var endPoint = Offset(size.width / 2, size.height);
+    path.quadraticBezierTo(
+        controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
+    path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
     return path;
   }
