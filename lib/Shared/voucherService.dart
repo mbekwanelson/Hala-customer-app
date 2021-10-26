85921c10aaa855055c211bd32f294d736b6e9aa9
siyathokoza cyril mbatha <siyathokozacmbatha@gmail.com>
2021-10-27 00:54:27 +0200

    migrated imagePicker to imagePicker()

--- lib/Shared/voucherService.dart
+++ lib/Shared/voucherService.dart
@@ -2,7 +2,8 @@ import 'package:image_picker/image_picker.dart';
 
 class voucherService {
   dynamic chooseFile() async {
-    return await ImagePicker.pickImage(source: ImageSource.gallery)
+    return await ImagePicker()
+        .pickImage(source: ImageSource.gallery)
         .then((image) {
       return image;
     });
